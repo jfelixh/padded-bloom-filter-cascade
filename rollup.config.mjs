f3d6b95b4b00aa2591baa0e17783f86a9fbd1cc5
crustypluto19 <evanchristopher1907@gmail.com>
2025-01-21 17:50:54 +0100

    refactor: update output directory structure for TypeScript and Rollup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,19 +2,13 @@ import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
-import { readFile } from "fs/promises";
 import dts from "rollup-plugin-dts";
-
-// Read package.json
-const pkg = JSON.parse(
-  await readFile(new URL("./package.json", import.meta.url))
-);
 
 const config = [
   {
     input: "src/index.ts",
     output: {
-      dir: "dist",
+      dir: "dist/esm",
       format: "esm",
       sourcemap: true,
       preserveModules: true,
@@ -23,8 +17,6 @@ const config = [
     plugins: [
       typescript({
         tsconfig: "./tsconfig.json",
-        declaration: true,
-        declarationDir: "dist/types",
       }),
       resolve({
         preferBuiltins: true,
@@ -36,10 +28,8 @@ const config = [
   {
     input: "src/index.ts",
     output: {
-      dir: "dist/types",
+      file: "dist/index.d.ts",
       format: "esm",
-      preserveModules: true,
-      preserveModulesRoot: "src",
     },
     plugins: [dts()],
   },
