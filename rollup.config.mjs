e9f80bb897f10982fcc8ed8203237a58e2d58915
Evan Christopher <evanchristopher1907@gmail.com>
2025-01-22 02:55:34 +0100

    Refactor Rollup config (#6)
    * feat: configure Rollup for bundling
    
    * refactor: Rollup configuration
    
    * chore: add 'dist' to .gitignore
    
    * refactor: update output directory structure for TypeScript and Rollup
    
    * fix: enable comments in TS output
    
    * chore: enhance Rollup dts plugin configuration

---
+++ rollup.config.mjs
@@ -0,0 +1,45 @@
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+
+const config = [
+  {
+    input: "src/index.ts",
+    output: {
+      dir: "dist/esm",
+      format: "esm",
+      sourcemap: true,
+      preserveModules: true,
+      preserveModulesRoot: "src",
+    },
+    plugins: [
+      typescript({
+        tsconfig: "./tsconfig.json",
+      }),
+      resolve({
+        preferBuiltins: true,
+      }),
+      commonjs(),
+      json(),
+    ],
+  },
+  {
+    input: "src/index.ts",
+    output: {
+      file: "dist/index.d.ts",
+      format: "esm",
+    },
+    plugins: [
+      dts({
+        respectExternal: true,
+        compilerOptions: {
+          removeComments: false,
+        },
+      }),
+    ],
+  },
+];
+
+export default config;
